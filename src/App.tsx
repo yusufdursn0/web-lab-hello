e2e4e6e3f925bc6b04f7a8f8c0b4e7fde99aeecd
Yusuf Dursun <yusufdursn0@gmail.com>
2026-04-01 02:09:43 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Yusuf</p>
+      <p>Ogrenci No: [230541104]</p>
+    </div>
+  )
+}
+
+export default App
