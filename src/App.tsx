ff32cc13d261ac3d28092ff53861d8e38f8d4a10
Yusuf Dursun <yusufdursn0@gmail.com>
2026-04-01 02:11:41 +0300

    feat: add personal details to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
       <h2>LAB-1</h2>
       <p>Ad Soyad: Yusuf</p>
       <p>Ogrenci No: [230541104]</p>
+      <p>Bolum: Yazılım Mühendisliği</p>
+      <p>Hobiler:Oyun Oynamak</p>
     </div>
   )
 }
