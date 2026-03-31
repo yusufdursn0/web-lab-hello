418c6e0fbe0eee3618b989eef58c1e18942f5ba9
Yusuf Dursun <yusufdursn0@gmail.com>
2026-04-01 02:16:10 +0300

    style: update page styling and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
+import './App.css'
+
 function App() {
   return (
-    <div>
-      <h1>Web Tasarimi ve Programlama</h1>
+    <div className="container">
+      <h1>Web Tasarimi ve  Programlama</h1>
       <h2>LAB-1</h2>
-      <p>Ad Soyad: Yusuf</p>
-      <p>Ogrenci No: [230541104]</p>
-      <p>Bolum: Yazılım Mühendisliği</p>
-      <p>Hobiler:Oyun Oynamak</p>
+
+
+      <p><strong>Ad Soyad:</strong> Yusuf Dursun</p>
+      <p><strong>Ogrenci No:</strong> 230541104</p>
+      <p><strong>Bolum:</strong> Yazılım Mühendisliği</p>
+      <p><strong>Hobiler:</strong> Oyun Oynamak</p>
+      <p><strong>Kisa Tanitim:</strong> Modern web gelistirme ogreniyorum.</p>
     </div>
   )
 }
